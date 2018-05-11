3dce77a3bdbe9a3f56ea5122e893716ff7420ec1
Matt Roberts <matt@clause.io>
2018-05-11 11:56:16 +0100

    Changes for deployment via IBP samples page

---
+++ .bluemix/metadata.js
@@ -0,0 +1,63 @@
+module.exports = {
+  'clause-perishable-goodse': {
+    'completed_steps': [
+      // Step 1 - install prerequisites, provision blockchain network and Cloudant wallet.
+      {
+        // name of step, can be anything. appears in toast when there is an error
+        'step': 'started',
+
+        // helps pace the progress bar (seconds)
+        'next_step_expected_duration_s': 5 * 60,
+
+        // ui will give up on sample and log an error after timeout
+        'next_step_timeout_s': 8 * 60
+      },
+      // Step 2 - deploy contracts (Fabric chaincodes, Composer business networks).
+      {
+        'step': 'deploy_contracts',
+        'next_step_expected_duration_s': 2 * 60,
+        'next_step_timeout_s': 5 * 60
+      },
+      // Step 3 - deploy REST servers.
+      {
+        'step': 'deploy_rest_servers',
+        'next_step_expected_duration_s': 2 * 60,
+        'next_step_timeout_s': 5 * 60
+      },
+      // Step 4 - deploy applications.
+      {
+        'step': 'deploy_apps',
+        'next_step_expected_duration_s': 2 * 60,
+        'next_step_timeout_s': 5 * 60
+      },
+      // Step 5 - gather the URLs for the REST servers.
+      {
+        'step': 'gather_rest_server_urls',
+        'next_step_expected_duration_s': 1 * 60,
+        'next_step_timeout_s': 2 * 60
+      },
+      // Step 6 - gather the URLs for the applications.
+      {
+        'step': 'gather_app_urls',
+        'next_step_expected_duration_s': 1 * 60,
+        'next_step_timeout_s': 2 * 60
+      },
+      // Step 7 - start the REST servers.
+      {
+        'step': 'start_rest_servers',
+        'next_step_expected_duration_s': 2 * 60,
+        'next_step_timeout_s': 5 * 60
+      },
+      // Step 8 - start the applications.
+      {
+        'step': 'start_apps',
+        'next_step_expected_duration_s': 2 * 60,
+        'next_step_timeout_s': 5 * 60
+      },
+      // Last step - complete!
+      {
+        'step': 'sample_up',
+      }
+    ]
+  }
+};
