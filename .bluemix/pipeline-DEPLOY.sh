caa731ae14e8c3bdafe9b71afb84da3b522765a4
Matt Roberts <code@rbrts.uk>
2018-09-28 17:34:21 +0100

    FIx composer-rest-server docker at 0.19.5

--- .bluemix/pipeline-DEPLOY.sh
+++ .bluemix/pipeline-DEPLOY.sh
@@ -174,7 +174,7 @@ function deploy_composer_rest_server {
     CF_APP_NAME=rest-${BUSINESS_NETWORK_NAME}
     cf push \
         ${CF_APP_NAME} \
-        --docker-image @ibmblockchain/composer-rest-server:${COMPOSER_VERSION} \
+        --docker-image @ibmblockchain/composer-rest-server:0.19.5 \
         --random-route \
         -i 1 \
         -m 256M \
